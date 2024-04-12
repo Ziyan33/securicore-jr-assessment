f934ef15a9a0ab0c8b6d8c66471737adee68db2d
Ziyan33 <zliu169@my.centennialcollege.ca>
2024-04-12 05:16:26 -0400

    heroku

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'weather_app';
-$user = 'root';
-$pass = '';
+$host = 'us-cluster-east-01.k8s.cleardb.net';
+$db = 'heroku_d46522caa835ee2';
+$user = 'b92e9267ad347b';
+$pass = 'c5191839';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
